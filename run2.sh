02be977801d8a8770ddbaa3b52c0bbfbd3ecda2f
Manuel Dipolt <manuel@linux-home.at>
2019-09-14 14:36:26 +0200

    solve issue with gnuradio windows

---
+++ run2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# hack but works
+xhost +
+
+sudo docker run \
+  -it --rm --privileged \
+  -e PULSE_SERVER=unix:/run/user/1000/pulse/native \
+  -v /dev/bus/usb:/dev/bus/usb \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -e DISPLAY=unix$DISPLAY \
+  -u root \
+  -v /run/user/1000/pulse:/run/user/1000/pulse \
+  -v /home/xeniter/Schreibtisch/LIME/limesdr_docker/mnt:/mnt/ \
+  limesdr-toolkit "$@"
+
